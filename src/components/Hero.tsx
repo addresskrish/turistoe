9eee9e8e4b2cdd424c5ae3e75777b3904ebb2626
KRISH <addresskrish@gmail.com>
2025-11-17 02:37:04 +0530

    mid of navbar

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+import Navbar from "./Navbar"
+
+const Hero = () => {
+  return (
+    <section className='bg-hero-bg h-screen'>
+        <Navbar />
+    </section>
+  )
+}
+
+export default Hero
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-bg": "url('/src/assets/hero-bg.png')",
+      },
+    },
   },
   plugins: [],
-}
+};
